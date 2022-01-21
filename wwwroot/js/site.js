6c6415e3c55fbd3ac1c7e8340048edb39f6f5457
bjarkt <bjarke.tobiesen@gmail.com>
2022-01-21 21:19:41 +0100

    Disable button and input when loading

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,28 +1,43 @@
 const baseUrl = "/api/v1";
 
-const fromOaDateForm = document.getElementById("oaDates_FromOaDateForm");
-fromOaDateForm.onsubmit = onSubmitOaDate;
-
-const toOaDateForm = document.getElementById("oaDates_ToOaDateForm");
-toOaDateForm.onsubmit = onSubmitOaDate;
+const oaDateForms = [...document.querySelectorAll("form[data-form=oadates]")]
+for (const form of oaDateForms) {
+  form.onsubmit = onSubmitOaDate;
+}
 
 async function onSubmitOaDate(event) {
   event.preventDefault()
-  const path = event.target.dataset.path;
-  const formData = new FormData(event.target);
+  const form = event.target;
+  const path = form.dataset.path;
+  const formData = new FormData(form);
+  const elementsToDisable = [...form.querySelectorAll("input:not([disabled])"), ...form.querySelectorAll("button[type=submit]")]
   try {
     const urlSearchParams = new URLSearchParams(formData);
+    setDisabled(elementsToDisable, true);
     const response = await fetch(`${baseUrl}/oadate/${path}?${urlSearchParams}`);
     const body = await response.json();
-    const output = document.getElementById(`${event.target.id}_output`);
+    const outputField = form.querySelectorAll("input[name=output]")[0]
     if (!response.ok) {
       const error = body.error ?? Object.values(body.errors).join(" ");
-      output.value = error;
+      outputField.value = error;
     } else {
-      output.value = body.output;
+      outputField.value = body.output;
     }
   } catch (error) {
     alert(error)
     console.error(error)
+  } finally {
+    setDisabled(elementsToDisable, false);
+  }
+}
+
+/**
+ * 
+ * @param {NodeList} nodeList 
+ * @param {bool} disabledStatus 
+ */
+function setDisabled(nodeList, disabledStatus) {
+  for (const node of nodeList) {
+    node.disabled = disabledStatus
   }
 }
