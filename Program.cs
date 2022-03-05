17386c1cb3ee74146f202f5f587b2200a3348908
bjarkt <688414+bjarkt@users.noreply.github.com>
2022-03-05 15:14:51 +0100

    Use serilog

--- Program.cs
+++ Program.cs
@@ -1,4 +1,34 @@
+using Microsoft.AspNetCore.HttpLogging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+
+var loggerConfiguration = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+    .Enrich.FromLogContext();
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+if (!builder.Environment.IsEnvironment(Microsoft.Extensions.Hosting.Environments.Development))
+{
+    loggerConfiguration.WriteTo.Console(new CompactJsonFormatter());
+}
+else
+{
+    loggerConfiguration.WriteTo.Console();
+}
+Log.Logger = loggerConfiguration.CreateLogger();
+
+builder.Host.UseSerilog();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(Log.Logger);
+// builder.Services.AddHttpLogging(logging =>
+// {
+//     logging.LoggingFields = HttpLoggingFields.RequestProperties | HttpLoggingFields.Response;
+// });
 
 // Add services to the container.
 
@@ -18,7 +48,8 @@ app.UseSwaggerUI(c =>
     c.EnableTryItOutByDefault();
 });
 
-app.UseHttpLogging();
+// app.UseHttpLogging();
+app.UseSerilogRequestLogging();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
